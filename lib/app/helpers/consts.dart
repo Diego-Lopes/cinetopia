1d32e1aa9523998842558dd3890857aea3d56db4
Diego-Lopes <diegolopes2009@hotmail.com>
2026-07-13 13:55:21 -0400

    ♻️refactor: added principle of SOLID

---
+++ lib/app/helpers/consts.dart
@@ -0,0 +1,10 @@
+import 'package:cinetopia/app/api_key.dart';
+
+/**criar const de url e header */
+const String popularMoviesUrl =
+    "http://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+
+const requestHeader = {
+  'accept': 'application/json',
+  'Authorization': 'Bearer $apiKey',
+};
